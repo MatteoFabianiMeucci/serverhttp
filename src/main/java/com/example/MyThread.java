93bb2a03b18a0a9f08171e616ea010c8e29f46bc
lai2-24 <lai2-24@itismeucci.com>
2024-12-10 14:08:42 +0100

    Aggiunta file.txt, index.html, risposte HTTP

--- src/main/java/com/example/MyThread.java
+++ src/main/java/com/example/MyThread.java
@@ -2,19 +2,23 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class MyThread extends Thread{
     private Socket s;
-
-    public MyThread(Socket s){
+    public MyThread(Socket s) throws IOException{
         this.s = s;
     }
 
     public void run(){
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()))) {
+        try {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
             String firstLine = in.readLine();
@@ -32,16 +36,54 @@ public class MyThread extends Thread{
             } while (!header.isEmpty());
 
             System.out.println("Richiesta terminata");
+            String responseBody;
+            switch (resource) {
+                case "/":
 
-            String responseBody = "Ciao a tutti";
-            out.writeBytes("HTTP/1.1 200 OK\n");
-            out.writeBytes("Content-Type: text/plain\n");
-            out.writeBytes("Content-Lenght: " + responseBody.length() + "\n");
-            out.writeBytes("\n");
-            out.writeBytes(responseBody);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+                case "/index.html":
+                    File file = new File("htdocs/index.html");
+                    InputStream input = new FileInputStream(file);
+                    
+
+                    out.writeBytes("HTTP/1.1 200 OK\n");
+                    out.writeBytes("Content-Type: text/html\n");
+                    out.writeBytes("Content-Length: " + file.length() + "\n");
+                    out.writeBytes("\n");
+                    
+                    byte[] buf = new byte[8192];
+                    int n;
+
+                    while ((n = input.read(buf)) != -1) {
+                        out.write(buf, 0, n);
+                    }
+                    input.close();
+                    break;
+
+                case "/file.txt":
+                    File file2 = new File("htdocs/file.txt");
+                    InputStream input2 = new FileInputStream(file2);
+                    out.writeBytes("HTTP/1.1 200 OK\n");
+                    out.writeBytes("Content-Type: text/plain\n");
+                    out.writeBytes("Content-Length: " + file2.length() + "\n");
+                    out.writeBytes("\n");
+                    byte[] buf2 = new byte[8192];
+                    int n2;
+
+                    while ((n2 = input2.read(buf2)) != -1) {
+                        out.write(buf2, 0, n2);
+                    }
+                    input2.close();
+                    break;
+            
+                default:
+                    out.writeBytes("HTTP/1.1 404 Not found\n");
+                    out.writeBytes("Content-Length: 0" + "\n");
+                    out.writeBytes("\n");
+                    break;
+            }
+            s.close();
+        } catch (Exception e) {
+            // TODO: handle exception
         }
     }
-}
+    }
